a83a7aa6d37ef25f038440d119ecf059cb730f4b
eazybytes <tutor@eazybytes.com>
2022-10-12 18:24:36 +0530

    Auto Scaling, Healing & Deployment using Kubernetes

--- section_13/loans/src/main/resources/data.sql
+++ section_13/loans/src/main/resources/data.sql
@@ -13,14 +13,14 @@ CREATE TABLE `loans` (
 );
 
 INSERT INTO `loans` ( `customer_id`, `start_dt`, `loan_type`, `total_loan`, `amount_paid`, `outstanding_amount`, `create_dt`)
- VALUES ( 1, '2020-10-13', 'Home', 200000, 50000, 150000, '2020-10-13');
+ VALUES ( 1, CURDATE()-250, 'Home', 200000, 50000, 150000, CURDATE()-250);
  
 INSERT INTO `loans` ( `customer_id`, `start_dt`, `loan_type`, `total_loan`, `amount_paid`, `outstanding_amount`, `create_dt`)
- VALUES ( 1, '2020-06-06', 'Vehicle', 40000, 10000, 30000, '2020-06-06');
+ VALUES ( 1, CURDATE()-376, 'Vehicle', 40000, 10000, 30000, CURDATE()-376);
  
 INSERT INTO `loans` ( `customer_id`, `start_dt`, `loan_type`, `total_loan`, `amount_paid`, `outstanding_amount`, `create_dt`)
- VALUES ( 1, '2021-02-14', 'Home', 50000, 10000, 40000, '2018-02-14');
+ VALUES ( 1, CURDATE()-549, 'Home', 50000, 10000, 40000, CURDATE()-549);
 
 INSERT INTO `loans` ( `customer_id`, `start_dt`, `loan_type`, `total_loan`, `amount_paid`, `outstanding_amount`, `create_dt`)
- VALUES ( 1, '2018-02-14', 'Personal', 10000, 3500, 6500, '2018-02-14');
+ VALUES ( 1, CURDATE()-122, 'Personal', 10000, 3500, 6500, CURDATE()-122);
  
